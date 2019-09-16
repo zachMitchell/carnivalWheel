a087941f6465f09a2848a309040a1bfdf56d6b58
zachMitchell <31785316+zachMitchell@users.noreply.github.com>
2019-09-15 22:33:00 -0400

    animation is a-go! Everything in the animation engine is her e except for smooth sways. Gonna do that next.

--- wheelFuncs.js
+++ wheelFuncs.js
@@ -13,6 +13,8 @@ var wheelFuncs = {
         wInst.loop = undefined;
         wInst.percent = 0;
         wInst.percentIncrease = 0;
+        wInst.currAnimation = undefined;
+        wInst.currMilliseconds = 0; //For animations, once this reaches a max, it can be cleared to 0.
     },
 
     spin:function(wInst,speed = 2,pegPower = .02,axelDrag = .001){
@@ -40,16 +42,51 @@ var wheelFuncs = {
 
     },
 
-    /*Move the spinner in different directions dynamically based on 3 things:
-    percent: Location where it will end up
-    time: milliseconds for the animation duration
-    sfx: sound you want to make when done.
-    
-    All arguments will be inserted in to an array, and multiple arrays can go in one function call.
-    The first argument can optionally be a bool stating if you want to reset the wheel's posistion.
+    /*Move the spinner in different directions dynamically based on a configuration provided:
+    The config is an object with the following properties:
+    wheel: the target wheel
+    sectors: an array of arrays, each containing some of the following indexes:
+        [0] percent to which we stop on - float
+        [1] duration in milliseconds - float
+        [2] allow tick to be detected - bool
+        [3] append percent instead of exact location - bool
+        [4] function to execute upon completion
     */
-    animate:function(){
+    animate:function*(config){
+        //Loop until we pass through everything:
+        for(var i of config.sectors){
+            config.wheel.currMilliseconds = 0;
+            var previousPercent = config.wheel.percent;
+            // console.log([(i[0] - previousPercent) / i[1],previousPercent,i[0],i[1]]);
+            config.wheel.loop = setInterval(()=>{
+                config.wheel.currMilliseconds+=16.6;
+                var targetChange = 0;
+                //If we want to append percentage:
+                if(i[3]) targetChange = i[0] / (i[1] / 16.6);
+                else targetChange = (i[0] - previousPercent) / (i[1] / 16.6);
+                // console.log(targetChange);
 
+                config.wheel.percent+=targetChange;
+                config.wheel.wheel.draw(config.wheel.percent,targetChange < 0);
+                var currTick = config.wheel.wheel.pegSet.detectPoint();
+                if(i[2] && config.wheel.lastTick < 50 && currTick > 50){
+                    //Tick sound as defined in the main page.
+                    tickSound.currentTime = 0;
+                    if(tickSound.paused) tickSound.play();
+                }
+                config.wheel.lastTick = currTick;
+
+                if(config.wheel.currMilliseconds >= i[1]){
+                    clearInterval(config.wheel.loop);
+                    //execute a function if it's available
+                    if(typeof i[4] == 'function') i[4](config.wheel);
+                    //NEXT sector!
+                    config.wheel.currAnimation.next();
+                }
+
+            },16.6);
+            yield; //To be honest, we don't need to return anything special, we just need to know when to proceed to the next section.
+        }
     }
 
 }

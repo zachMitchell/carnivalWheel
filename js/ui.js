cc076ca59bc4ec5d9c02c49ede8d2f8479e94b1f
Zachary <31785316+zachMitchell@users.noreply.github.com>
2019-12-01 17:23:35 -0500

    Continued worked on loading assets. This was harder than expected :P
    Safari is really the outlier in the situation, i might consider just returning the data url for it if there's no way to do it in-browser :(

---
+++ js/ui.js
@@ -0,0 +1,63 @@
+//Made by Zachary Mitchell in 2019!
+/*This file aims to handle several things one watches on screen.
+it is extremely general, so as it's being written to, other pieces will probably break into separate files...*/
+
+//Dependencies: confetti.js
+var ui = {
+    confettiPieces:[],
+    loadedConfetti:0,
+    //Links to all of these assets are here, then loaded in the list.
+    assetLinks:{
+        //Sounds:
+        audio:{
+            tickSound:'tick5.mp3',
+            whoopSfx:'Whoop.mp3',
+            drmRool:'DrumRollQuick.mp3',
+            cymbalSfx:'Cymbal.mp3',
+            confettiSfx:'noiseMaker.mp3',
+            wooshSmack:'wooshSmack.mp3',
+        },
+        //Images:
+        img:{
+            smileHappy:'smileHappy.svg',
+            spiralAxel:'spiralAxel.svg',
+            starAxel:'starAxel.svg',
+            heartAxel:'heartAxel.svg'
+        }
+    },
+    //Actual elements go here.
+    assetList:{
+        audio:{},
+        img:{},
+    },
+    //Load everything into the asset list where each can be called by name
+    loadAssets:function(onAssetLoad){
+
+        for(var i in this.assetLinks){
+            for(var j in this.assetLinks[i]){
+                //Load the thing!
+                ui.loadSpecificAsset(j,i,'assets/'+i+'/'+this.assetLinks[i][j]);
+            }
+        }
+
+
+        //Load confetti; this isn't required up front:
+        var rainbow = 'ROYGBIV';
+        for(var i = 0;i<rainbow.length;i++){
+            this.confettiPieces[i] = document.createElement('img');
+            this.confettiPieces[i].src = 'assets/img/confetti/conf'+rainbow[i]+'.svg';
+        }
+    },
+    //Load a specific asset, required here because variables can't be transferred over promise.then
+    loadSpecificAsset: async function(objName,elementType,url){
+        //Retrieve everything through blobs; this is done to keep those dasterdly stubborn audio clips in memory (and not load everytime... LOOKING AT YOU SAFARI >:( )
+            var query = await fetch(url);
+            //Grab the asset type from the index before the filename
+            var newAsset = document.createElement(elementType);
+            var assetData = await query.blob();
+            // console.log(assetData);
+            //Trigger onload (images only)
+            newAsset.src = URL.createObjectURL(assetData);
+            ui.assetList[elementType][objName] = newAsset;
+    }
+}
